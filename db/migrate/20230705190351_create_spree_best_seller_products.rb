fe6912eccc3ea278555d4c32c2313f2b99696b51
jeffreyrene <jeffreyrene575@gmail.com>
2022-04-25 00:00:00 +0500

    feat: update sort by modal.html

---
+++ db/migrate/20230705190351_create_spree_best_seller_products.rb
@@ -0,0 +1,9 @@
+class CreateSpreeBestSellerProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_view :spree_best_seller_products
+  end
+end
+
+# touched on 2025-05-22T19:15:38.007497Z
+# touched on 2025-05-22T20:44:44.575540Z
+# touched on 2025-05-22T21:58:11.116161Z
