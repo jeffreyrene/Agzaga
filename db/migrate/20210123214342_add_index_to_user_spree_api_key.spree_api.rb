aa14f2a0102681fe1c795ce6e5560cb80b020df1
jeffreyrene <jeffreyrene575@gmail.com>
2022-08-22 00:00:00 +0500

    feat: update solidus long white

---
+++ db/migrate/20210123214342_add_index_to_user_spree_api_key.spree_api.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+# This migration comes from spree_api (originally 20131017162334)
+
+class AddIndexToUserSpreeApiKey < ActiveRecord::Migration[4.2]
+  def change
+    unless defined?(User)
+      add_index :spree_users, :spree_api_key
+    end
+  end
+end
+
+# touched on 2025-05-22T20:31:42.864918Z
