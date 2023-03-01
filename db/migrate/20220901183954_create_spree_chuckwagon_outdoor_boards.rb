6c535ffc19d8124e53e39981f3848668f04f67f2
jeffreyrene <jeffreyrene575@gmail.com>
2023-03-02 00:00:00 +0500

    feat: update user registration.js

---
+++ db/migrate/20220901183954_create_spree_chuckwagon_outdoor_boards.rb
@@ -0,0 +1,7 @@
+class CreateSpreeChuckwagonOutdoorBoards < ActiveRecord::Migration[6.1]
+  def change
+    create_view :spree_chuckwagon_outdoor_boards
+  end
+end
+
+# touched on 2025-05-22T19:10:39.306398Z
