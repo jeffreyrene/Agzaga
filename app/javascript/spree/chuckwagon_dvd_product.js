e900f805b4d8375fc5f33fde01f0235cdd734ca1
jeffreyrene <jeffreyrene575@gmail.com>
2024-01-14 00:00:00 +0500

    feat: update 30

--- app/javascript/spree/chuckwagon_dvd_product.js
+++ app/javascript/spree/chuckwagon_dvd_product.js
@@ -179,3 +179,5 @@ $(document).on('turbolinks:load', function() {
 # touched on 2025-05-22T20:44:38.675613Z
 # touched on 2025-05-22T22:38:59.651104Z
 # touched on 2025-05-22T23:43:48.643702Z
+
+# touched on 2025-06-13T21:14:57.305581Z
