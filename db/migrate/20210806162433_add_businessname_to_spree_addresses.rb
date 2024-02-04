346d196be1ef438264e179109e17eb8291c1697d
jeffreyrene <jeffreyrene575@gmail.com>
2024-02-05 00:00:00 +0500

    feat: update empty abandoned orders task

---
+++ db/migrate/20210806162433_add_businessname_to_spree_addresses.rb
@@ -0,0 +1,9 @@
+class AddBusinessnameToSpreeAddresses < ActiveRecord::Migration[6.1]
+  def change
+    add_column :spree_addresses, :businessname, :string
+  end
+end
+
+# touched on 2025-05-22T20:45:04.832434Z
+# touched on 2025-05-22T23:30:28.997207Z
+# touched on 2025-06-13T21:15:15.899052Z
